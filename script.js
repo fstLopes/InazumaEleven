e09a340c4446221e0bb40f048dec1f1d1dac4596
Lopes. <mlopesdiscord@gmail.com>
2025-10-12 08:57:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,94 @@
+//ENDOU
+function mudarEndou() {
+    const texto = document.getElementById("textoEndou");
+    const imagem = document.getElementById("Endou");
+    texto.textContent = "Mão Fantasma, Elemento: Montanha ⛰️; Punho de Fogo, Elemento: Fogo 🔥; Punho Explosivo, Elemento: Fogo 🔥; Mão Demoniaca, Elemento: Montanha ⛰️; Punho da Justiça, Elemento: Vento 🌫️; Martelo da Ira, Elemento: Montanha ⛰️; Mão Dimensional, Elemento: Floresta 🌳; Captura Divina, Elemento: Montanha ⛰️.";
+    imagem.src = "imagens/EndouJapao.png";
+    imagem.alt = "Endou";
+}
+document.getElementById("EDU").addEventListener("click", mudarEndou);
+
+function mudarEndou2() {
+    const texto = document.getElementById("textoEndou");
+    const imagem = document.getElementById("Endou");
+    texto.textContent = "O protagonista da série, Endou é o goleiro e capitão do time Raimon. Conhecido por seuotimismo inabalável e espírito de liderança, ele é movido por sua paixão pelo futebol e pelo legado de seu avô. Sempre pronto para encorajar seus companheiros, Endou é o coração da equipe e famoso por suas defesas espetaculares, como a Mão Fantasma.";
+    imagem.src = "imagens/EndouRaimon.png";
+    imagem.alt = "Endou";
+}
+document.getElementById("EDUinfo").addEventListener("click", mudarEndou2);
+
+//GOUENJI
+function mudarGouenji() {
+    const texto = document.getElementById("textoGouenji");
+    const imagem = document.getElementById("Gouenji");
+    texto.textContent = "Tornado de Fogo, Elemento: Fogo 🔥\n; Furacão Dragão, Elemento: Fogo 🔥\n; Gol Relâmpago, Elemento: Vento 🌫️\n; Gol Relâmpago 1, Elemento: Vento 🌫️\n; Catavento de Fogo, Elemento: Fogo 🔥\n; Relâmpago Destruidor, Elemento: Vento 🌫️\n; Tempestade Explosiva, Elemento: Fogo 🔥\n; Fogo Cruzado, Elemento: Fogo/Vento 🔥🌫️\n; A Terra, Elemento: Montanha ⛰️\n; Hélice Dinamite, Elemento: Fogo 🔥\n; Tempestade do Tigre, Elemento: Fogo 🔥\n; Grande Incendio, Elemento: Fogo 🔥\n; Corrente de Jato, Elemento: Vento 🌫️";
+    imagem.src = "imagens/GouenjiJapao.png";
+    imagem.alt = "Gouenji";
+}
+document.getElementById("GOJ").addEventListener("click", mudarGouenji);
+
+function mudarGouenji2() {
+    const texto = document.getElementById("textoGouenji");
+    const imagem = document.getElementById("Gouenji");
+    texto.textContent = "Um dos atacantes mais poderosos da série, Gouenji é calmo, sério e extremamente talentoso. Inicialmente relutante em voltar ao futebol por motivos pessoais, ele retorna como peça - chave da Raimon.Seu chute especial, o     Tornado de Fogo, é lendário, e sua presença inspira respeito.";
+    imagem.src = "imagens/GouenjiRaimon.png";
+    imagem.alt = "Gouenji";
+}
+document.getElementById("GOJinfo").addEventListener("click", mudarGouenji2);
+
+//KIDOU
+function mudarKidou() {
+    const texto = document.getElementById("textoKidou");
+    const imagem = document.getElementById("Kidou");
+    texto.textContent = "Pinguins Imperiais n2, Elemento: Floresta 🌳; Duplo Boost, Elemento: Fogo 🔥; Relâmpago Destruidor, Elemento: Vento 🌫️; Bola Ilusoria, Elemento: Floresta 🌳; Zona Mortal 2, Elemento: Floresta 🌳; Campos Assassinos, Elemento: Floresta 🌳; Pinguins Imperiais n3, Elemento: Floresta 🌳; Big Bang, Elemento: Fogo 🔥.";
+    imagem.src = "imagens/68794.png";
+    imagem.alt = "Kidou usando suas técnicas";
+}
+document.getElementById("KDU").addEventListener("click", mudarKidou);
+
+function mudarKidou2() {
+    const texto = document.getElementById("textoKidou");
+    const imagem = document.getElementById("Kidou");
+    texto.textContent = "Estrategista brilhante e ex-capitão da equipe da Teikoku Gakuen, Kidou é conhecido por sua inteligência tática e habilidades como meio-campista. Usa uma bandana que simboliza sua conexão com seu irmão adotivo. Frio e calculista no começo, ele se torna um aliado valioso da Raimon.";
+    imagem.src = "imagens/Teikoku.png";
+    imagem.alt = "Kidou usando suas técnicas";
+}
+document.getElementById("KDUinfo").addEventListener("click", mudarKidou2);
+
+//APHRODI
+function mudarAphrodi() {
+    const texto = document.getElementById("textoAphrodi");
+    const imagem = document.getElementById("Aphrodi");
+    texto.textContent = "Tempo Celestial, Elemento: Vento 🌫️; Asas Celestiais, Elemento: Vento 🌫️; Ruptura Divina, Elemento: Vento 🌫️; Ruptura do Cáos, Elemento: Floresta 🌳.";
+    imagem.src = "imagens/AphrodiCoreia.png";
+    imagem.alt = "Aphrodi usando suas técnicas";
+}
+document.getElementById("AFD").addEventListener("click", mudarAphrodi);
+
+function mudarAphrodi2() {
+    const texto = document.getElementById("textoAphrodi");
+    const imagem = document.getElementById("Aphrodi");
+    texto.textContent = "Aphrodi é um jogador elegante e quase celestial, conhecido por seu estilo refinado e chute especial Deus Knows. Com aparência andrógina e comportamento sereno, ele joga pela equipe Zeus. Apesar de inicialmente rival, demonstra ter um forte senso de justiça e respeito pelo futebol verdadeiro.";
+    imagem.src = "imagens/AphrodiZeus.png";
+    imagem.alt = "Aphrodi usando suas técnicas";
+}
+document.getElementById("AFDinfo").addEventListener("click", mudarAphrodi2);
+
+//FUBUKI
+function mudarFubuki() {
+    const texto = document.getElementById("textoFubuki");
+    const imagem = document.getElementById("Fubuki");
+    texto.textContent = "Vendaval Eterno, Elemento: Vento 🌫️; Chão de Gelo, Elemento: Vento 🌫️; Lobo Lendario, Elemento: Vento 🌫️; Fogo Cruzado, Elemento: Vento/Fogo 🌫️🔥; Anjo de Neve, Elemento: Vento 🌫️; Besta Trovão, Elemento: Vento 🌫️; O Furacão, Elemento: Vento 🌫️; O Nascimento, Elemento: Vento 🌫️; Big Bang, Elemento: Fogo 🔥.";
+    imagem.src = "imagens/FubukiJapao.png";
+    imagem.alt = "Fubuki usando suas técnicas";
+}
+document.getElementById("FBK").addEventListener("click", mudarFubuki);
+
+function mudarFubuki2() {
+    const texto = document.getElementById("textoFubuki");
+    const imagem = document.getElementById("Fubuki");
+    texto.textContent = "Um jogador com uma história trágica, Fubuki é tanto atacante quanto defensor. Após perder seu irmão gêmeo Atsuya, ele desenvolve uma segunda personalidade, alternando entre o calmo Shirou e o agressivo Atsuya. Seu chute mais famoso é o Tempestade de Gelo. Ele representa a luta interna e superação pessoal.";
+    imagem.src = "imagens/FubukiHakuren.png";
+    imagem.alt = "Fubuki usando suas técnicas";
+}
+document.getElementById("FBKinfo").addEventListener("click", mudarFubuki2);
